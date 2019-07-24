1f7ebd4f8958d61541c2f26a372cfdc4ae4a69ce
Audrea Cook <audreacwork@gmail.com>
2019-07-24 19:51:26 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,5 +3,14 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
+  attr_accessor :emails
   
+  def initialize
+    parse(emails)
+  end
+  
+  def parse(emails)
+    emails_array = emails.split(" " || ",")
+    emails_array.uniq!
+  end
 end
